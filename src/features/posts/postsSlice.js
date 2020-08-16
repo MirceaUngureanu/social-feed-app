5e6dc9a2a003919ed040135bdcd37adcdaa10256
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-08-16 13:07:38 +0100

    fix: correct state id mutation in postsSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -28,7 +28,7 @@ const postsSlice = createSlice({
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.find(post => post.id = id)
+      const existingPost = state.find(post => post.id === id)
 
       if (existingPost) {
         existingPost.title = title
