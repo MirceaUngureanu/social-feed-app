2ab090e99c30b780c7ada0f321270ab705ea6147
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-08-18 14:53:08 +0100

    feat: Async load users on start using the fetchUsers thunk

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
+import { fetchUsers } from './features/users/usersSlice'
+
 import './api/server'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={ store }>
+      <App/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
