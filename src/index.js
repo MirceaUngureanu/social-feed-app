83baaeabd46036a39439f13cf700951634f1b13c
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-08-20 12:13:32 +0100

    feat: Load initial notifications

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 import { fetchUsers } from './features/users/usersSlice'
+import {fetchNotifications} from './features/notifications/notificationsSlice'
 
 import './api/server'
 
 store.dispatch(fetchUsers())
+store.dispatch(fetchNotifications())
 
 ReactDOM.render(
   <React.StrictMode>
