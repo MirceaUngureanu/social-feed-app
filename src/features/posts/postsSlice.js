6f52cb9d01aaa2a26db3e454688bdc2b6ac403ce
Mircea Ungureanu <mircea.c.ungureanu@gmail.com>
2020-08-21 11:53:20 +0100

    feat: memoize selectPostsByUser selector

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createSelector } from "@reduxjs/toolkit";
 import { client } from '../../api/client'
 
 const initialState = {
@@ -70,5 +70,10 @@ export const selectAllPosts = state => state.posts.posts
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find(post => post.id === postId)
+
+export const selectPostsByUser = createSelector(
+  [selectAllPosts, (state, userId) => userId],
+  (posts, userId) => posts.filter(post => post.user === userId)
+)
 
 export default postsSlice.reducer
